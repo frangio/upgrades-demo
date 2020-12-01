91e58e1a84acb5348d2959e7f23e7fe3110dd342
Francisco Giordano <frangio.1@gmail.com>
2020-12-01 15:34:38 -0300

    explain gap better and provide simpler alternative

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -54,13 +54,44 @@ contract TokenV2Good is ERC20CappedUpgradeable, TokenState, ERC20PausableUpgrade
     }
 }
 
-// more future proof alternative
+// TokenState can't grow, can only append to the end of the inheritance list
+
+contract TokenStateExtension {
+    uint z1;
+    uint z2;
+}
+
+contract TokenV3 is ERC20CappedUpgradeable, TokenState, ERC20PausableUpgradeable, TokenStateExtension {
+
+    function initialize() public {
+        __ERC20_init("TokenV1", "TV1");
+        __ERC20Capped_init(10e18);
+        __ERC20Pausable_init();
+    }
+
+    function _beforeTokenTransfer(address from, address to, uint256 amount)
+        internal virtual override(ERC20CappedUpgradeable, ERC20PausableUpgradeable)
+    {
+        super._beforeTokenTransfer(from, to, amount);
+    }
+}
+
+// or a more future proof alternative can be used from the beginning
 // see Storage Gaps: https://docs.openzeppelin.com/contracts/3.x/upgradeable#storage_gaps
 
-contract TokenStateBetter {
+contract TokenStateBetterV1 {
     uint s;
 
     uint[50] private __gap;
+}
+
+contract TokenStateBetterV2 {
+    uint s;
+
+    uint z1;
+    uint z2;
+
+    uint[48] private __gap;
 }
 
 // caveat: upgrades plugins will complain if gap size is changed. this will be fixed in a future release
